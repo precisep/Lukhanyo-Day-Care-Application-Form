08645c09523b9d4d6974e654a5349969254cfda4
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2023-10-22 01:43:24 +0200

    Update application_form.py

--- application_form.py
+++ application_form.py
@@ -76,8 +76,7 @@ with st.form('data_form', clear_on_submit=True):
     # Display the GIF using Markdown with width set to 400
     st.markdown(
     f'<img src="data:gif;base64,{data_url}" alt="logo gif" width="685" loop>',
-    unsafe_allow_html=True,
-)
+    unsafe_allow_html=True,)
 
 
     # Child's information
